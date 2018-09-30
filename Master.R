6f38c127864337ba6c99475de15a955e88e04add
aspina7 <alexfspina@gmail.com>
2018-09-30 12:47:24 +0100

    Clean r-script to source other scripts

---
+++ Master.R
@@ -0,0 +1,9 @@
+
+#Run each of your scripts
+source("DataChecking.R")
+source("DataRecoding.R")
+source("DescriptiveAnalysis.R")
+source("ComparativeAnalysis.R")
+source("MergingDenominators.R")
+source("AnnualIncidenceRates.R") 
+source("IRR.R")
